8f680530ad11a1110f804c1a0013132567062c71
se733 <chavezsebas737@gmail.com>
2023-01-10 12:09:35 -0300

    tipos basicos

--- src/typescript/TiposBasicos.tsx
+++ src/typescript/TiposBasicos.tsx
@@ -1,17 +1,10 @@
 export const TiposBasicos = () => {
 
-    const nombre : string = 'Sebas'
-    const edad : number = 27
-    const estaActivo : boolean = true;
 
-    const poderes: string[] = []
-
-    
 
   return (
     <>
         <h2>tipos basicos</h2>
-        {nombre}, {edad}, {poderes}
     </>
     )
 }
