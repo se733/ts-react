289999f2fc4a03a76310409ce6cff8d79a32b893
se733 <chavezsebas737@gmail.com>
2023-01-11 18:39:17 -0300

    tipos de datos ts

--- src/typescript/TiposBasicos.tsx
+++ src/typescript/TiposBasicos.tsx
@@ -1,10 +1,28 @@
-export const TiposBasicos = () => {
+interface Persona {
+  nombre: string;
+  edad: number;
+}
 
+export const TiposBasicos = () => {
+  const personasListas = () => {
+    const persona: Persona = {
+      nombre: "Seba",
+      edad: 27,
+    };
+  };
+  console.log(personasListas);
+
+
+  
+  const sumar = ():number => {
+    return 2+1
+  }
 
 
   return (
     <>
-        <h2>tipos basicos</h2>
+      <h2>tipos basicos</h2>
+      {sumar()}
     </>
-    )
-}
+  );
+};
