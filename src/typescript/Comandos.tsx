b14aeeea1788f6e65a0de0437e8113a54ad20b1e
se733 <chavezsebas737@gmail.com>
2023-01-16 19:05:33 -0300

    restauración

--- src/typescript/Comandos.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Comandos = () => {
-  return (
-    <div>Comandos</div>
-  )
-}
--- src/typescript/Nomenclar.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const Nomenclar = () => {
-  return (
-    <div>Nomenclar</div>
-  )
-}
