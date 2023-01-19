618798dd1e78eaf698e444b590db1af5cf28decc
se733 <chavezsebas737@gmail.com>
2023-01-19 10:44:06 -0300

    Login y reducer

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,6 +1,32 @@
-import React from 'react'
+import React, { useReducer } from 'react'
+
+interface AuthState {
+    validando: boolean;
+    token: null | string;
+    username: string;
+    nombre: string;
+}
+
+
+const initialState: AuthState = {
+    validando: true,
+    token: null,
+    username: '',
+    nombre: ''
+}
+
+const authReducer = ():typeof AuthState => {
+    
+}
+
+
+
+
 
 export const Login = () => {
+
+
+    const [state, dispatch] = useReducer(reducer, initialState);
   return (
     <>
     <h1>Login</h1>
