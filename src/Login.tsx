0a9ad537ee240ae136d378f4da5b574158f9c991
se733 <chavezsebas737@gmail.com>
2023-01-23 18:34:06 -0300

    useReducer

--- src/Login.tsx
+++ src/Login.tsx
@@ -14,51 +14,80 @@ const initialState: AuthState = {
   nombre: "",
 };
 
-type AuthAction = { type: "logout" };
-
-const authReducer = (state: AuthState, action: AuthAction):  AuthState => {
-    switch (action.type) {
-        case 'logout':
-            return {
-                validando: false,
-                token: null,
-                username: '',
-                nombre: ''
-            }
-            default: return state;
-    
-       
-    }
+type LoginPayload = {
+  username: string;
+  nombre: string;
 };
 
+type AuthAction = { type: "logout" } | { type: "login"; payload: LoginPayload };
+
+const authReducer = (state: AuthState, action: AuthAction): AuthState => {
+  switch (action.type) {
+    case "logout":
+      return {
+        validando: false,
+        token: null,
+        username: "",
+        nombre: "",
+      };
+    case "login":
+      const { username, nombre } = action.payload;
+      return {
+        validando: false,
+        token: "123ABC",
+        username,
+        nombre,
+      };
+
+    default:
+      return state;
+  }
+};
 
 export const Login = () => {
-    const [state, dispatch] = useReducer(authReducer, initialState);
+  const [{ validando, token, nombre }, dispatch] = useReducer(
+    authReducer,
+    initialState
+  );
 
-    useEffect(() => {
-        setTimeout(() => {
-            dispatch({type: 'logout'})
-        },1500)
-    }, [])
-    
+  useEffect(() => {
+    setTimeout(() => {
+      dispatch({ type: "logout" });
+    }, 1500);
+  }, []);
 
-    if(state.validando) {
-        return (
-            <>
-                <h3>Login</h3>
-                <div className="alert alert-info">Validando...</div>
-            </>
-        )
-    }
+  if (validando) {
+    return (
+      <>
+        <h3>Login</h3>
+        <div className="alert alert-info">Validando...</div>
+      </>
+    );
+  }
 
+  const login = () => {
+    dispatch({
+      type: 'login',
+      payload: {
+        nombre : 'Sebas',
+        username: '123'
+      }
+    })
+  }
 
   return (
     <>
-      <div className="alert alert-danger">No autenticado...</div>
-      <div className="alert alert-success">Autenticado</div>
-      <button className="btn btn-primary">Login</button>
-      &nbsp;
-      <button className="btn btn-danger">Login</button>
+      {token ? (
+        <div className="alert alert-success">Autenticado como: {nombre} </div>
+      ) : (
+        <div className="alert alert-danger">No autenticado...</div>
+      )}
+
+      {token ? (
+        <button className="btn btn-danger">Logout</button>
+      ) : (
+        <button className="btn btn-primary" onClick={ login }>Login</button>
+      )}
     </>
   );
 };
