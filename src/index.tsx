c15e566bcc646e4901810b108f15b875a4bfbc8b
se733 <chavezsebas737@gmail.com>
2023-01-26 19:28:36 -0300

    creacion de useForm y final de typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import * as ReactDOMClient from "react-dom/client";
 
-import App from "./App";
-import { Contador } from './Contador';
-import { Login } from './Login';
-import { Usuarios } from './Usuarios';
+// import App from "./App";
+// import { Contador } from './Contador';
+// import { Login } from './Login';
+// import { Usuarios } from './Usuarios';
+import { Formularios } from './Formularios';
 
 const container = document.getElementById("root") as HTMLDivElement;
 
 ReactDOMClient.createRoot(container)
-.render(<Usuarios />);
+.render(<Formularios />);
